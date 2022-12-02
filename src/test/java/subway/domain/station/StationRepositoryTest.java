11234d2945004170d7c34e42f9fd5013d4ae9798
kjb95 <topmedia44@gmail.com>
2022-12-02 14:05:36 +0900

    test: 역 삭제하기에 대한 테스트코드 작성

--- src/test/java/subway/domain/station/StationRepositoryTest.java
+++ src/test/java/subway/domain/station/StationRepositoryTest.java
@@ -13,4 +13,11 @@ class StationRepositoryTest {
         StationRepository.addStation("충정로역");
         assertThatCode(() -> StationRepository.findStationByName("충정로역")).doesNotThrowAnyException();
     }
+
+    @Test
+    void 역_삭제하기() {
+        assertThatCode(() -> StationRepository.findStationByName("강남역")).doesNotThrowAnyException();
+        StationRepository.deleteStation("강남역");
+        assertThatThrownBy(() -> StationRepository.findStationByName("강남역")).isInstanceOf(IllegalArgumentException.class);
+    }
 }
