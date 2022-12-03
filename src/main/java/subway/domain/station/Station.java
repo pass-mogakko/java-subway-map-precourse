c69986c8d19167399a8c4db158263b51d6bc1a99
Isu Shin <isu010614@naver.com>
2022-12-03 22:05:10 +0900

    feat: 지하철 역 이름의 길이 검증하는 기능

---
+++ src/main/java/subway/domain/station/Station.java
@@ -0,0 +1,26 @@
+package subway.domain.station;
+
+import subway.message.ErrorMessage;
+
+public class Station {
+    private final String name;
+
+    private static final int MINIMUM_NAME_LENGTH = 2;
+
+    public Station(String name) {
+        validateNameLength(name);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void validateNameLength(String name) {
+        if(name.length() < MINIMUM_NAME_LENGTH ){
+            throw new IllegalArgumentException(ErrorMessage.MINIMUM_NAME_LENGTH);
+        }
+    }
+
+    // 추가 기능 구현
+}
