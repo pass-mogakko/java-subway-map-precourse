b24b48fbf9dc1d3889db8785c949ad954e6acb46
Isu Shin <isu010614@naver.com>
2022-12-03 22:00:46 +0900

    feat: 에러 메시지 추가

--- src/main/java/subway/message/ErrorMessage.java
+++ src/main/java/subway/message/ErrorMessage.java
@@ -1,7 +1,10 @@
 package subway.message;
 
 public class ErrorMessage {
-    private static final String ERROR = "[ERROR]";
-    public static final String NOT_EXIST_FUNCTION_KEY = ERROR + "기능이 존재하지 않는 숫자입니다.";
+    private static final String ERROR = "[ERROR] ";
+    public static final String NOT_EXIST_FUNCTION = ERROR + "선택할 수 없는 기능입니다.";
+    public static final String MINIMUM_NAME_LENGTH = ERROR + "지하철 역 이름은 두글자 이상이어야 합니다.";
+    public static final String ALREADY_EXISTING_STATION = ERROR + "이미 등록된 역 이름입니다.";
+    public static final String NOT_EXIST_STATION = ERROR + "존재하지 않는 역입니다.";
 
 }
