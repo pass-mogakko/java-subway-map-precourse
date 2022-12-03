5495b2182b0e677c13adb0eab546344ceba66ab5
Isu Shin <isu010614@naver.com>
2022-12-03 18:42:20 +0900

    feat: 메인화면 출력

--- src/main/java/subway/controller/MainController.java
+++ src/main/java/subway/controller/MainController.java
@@ -4,20 +4,27 @@ import subway.domain.Function;
 import subway.view.InputView;
 import subway.view.OutputView;
 
+import java.util.List;
+
 public class MainController {
 
-    public void view(){
-        OutputView.printMainView();
+    public void view() {
+        printMainView();
         final String function = readFunction();
     }
 
-    private String readFunction(){
-        try{
+    private String readFunction() {
+        try {
             String function = InputView.inputFunction();
-            return Function.get(function);
-        }catch (IllegalArgumentException e){
+            return Function.find(function);
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return readFunction();
         }
+    }
+
+    private void printMainView() {
+        List<String> functions = Function.getFunctions();
+        OutputView.printMainView(functions);
     }
 }
