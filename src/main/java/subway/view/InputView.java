820f92e382dfe1612a28996bcbd4f065b12ac2ea
Isu Shin <isu010614@naver.com>
2022-12-03 22:01:31 +0900

    feat: 역 이름 입력받는 기능

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -10,4 +10,16 @@ public class InputView {
         System.out.println("## 원하는 기능을 선택하세요.");
         return scanner.next();
     }
+
+    public static String inputRegisterStation(){
+        System.out.print(System.lineSeparator());
+        System.out.println("## 등록할 역 이름을 입력하세요.");
+        return scanner.next();
+    }
+
+    public static String inputDeleteStation(){
+        System.out.print(System.lineSeparator());
+        System.out.println("## 삭제할 역 이름을 입력하세요.");
+        return scanner.next();
+    }
 }
