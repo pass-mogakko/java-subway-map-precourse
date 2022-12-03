3fbd2c4e210407dc3818ef08e9e7284f592c0c57
Isu Shin <isu010614@naver.com>
2022-12-03 18:25:20 +0900

    feat: 기능에 대한 enum 생성

---
+++ src/main/java/subway/message/ErrorMessage.java
@@ -0,0 +1,7 @@
+package subway.message;
+
+public class ErrorMessage {
+    private static final String ERROR = "[ERROR]";
+    public static final String NOT_EXIST_FUNCTION_KEY = ERROR + "기능이 존재하지 않는 숫자입니다.";
+
+}
