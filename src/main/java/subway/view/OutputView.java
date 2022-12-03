20d5b60fc9c3a05c01ef92fa04038fa0b7add5e2
Isu Shin <isu010614@naver.com>
2022-12-03 22:02:41 +0900

    feat: 역 관리 화면 출력기능

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -3,12 +3,21 @@ package subway.view;
 import java.util.List;
 
 public class OutputView {
+    private static final String INFO = "[INFO]" + " ";
 
     public static void printMainView(List<String> functions){
         System.out.print(System.lineSeparator());
         System.out.println("## 메인 화면");
         for(String function : functions){
             System.out.println(function);
+        }
+    }
+
+    public static void printStationManageView(List<String> manages){
+        System.out.print(System.lineSeparator());
+        System.out.println("## 역 관리 화면");
+        for(String manage : manages){
+            System.out.println(manage);
         }
     }
 }
