eefb844370c6452fc325a5ba170d3ae21dd87015
Isu Shin <isu010614@naver.com>
2022-12-04 17:32:00 +0900

    style: 컨벤션 수정

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -3,29 +3,29 @@ package subway.view;
 import java.util.List;
 
 public class OutputView {
-    private static final String INFO = "[INFO]" + " ";
+    private static final String INFO = "[INFO] %s";
 
-    public static void printMainView(List<String> functions){
+    public static void printMainView(List<String> functions) {
         System.out.print(System.lineSeparator());
         System.out.println("## 메인 화면");
-        for(String function : functions){
+        for (String function : functions) {
             System.out.println(function);
         }
     }
 
-    public static void printStationManageView(List<String> manages){
+    public static void printStationManageView(List<String> manages) {
         System.out.print(System.lineSeparator());
         System.out.println("## 역 관리 화면");
-        for(String manage : manages){
+        for (String manage : manages) {
             System.out.println(manage);
         }
     }
 
-    public static void printStationList(List<String> stationList){
+    public static void printStationList(List<String> stationList) {
         System.out.print(System.lineSeparator());
         System.out.println("## 역 목록");
-        for(String station : stationList){
-            System.out.println(INFO + station);
+        for (String station : stationList) {
+            System.out.printf(INFO, station);
         }
     }
 }
