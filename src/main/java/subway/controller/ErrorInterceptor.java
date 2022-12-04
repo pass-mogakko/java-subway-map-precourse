1266d600108620c0aa917aa842e3c3e8e69e8b6e
yoondgu <doyou2ing@naver.com>
2022-12-04 16:52:30 +0900

    :sparkles:feat(controller): UI-8 예외 발생 시 에러 메시지 출력, 관리 화면으로 돌아가는 기능 구현

---
+++ src/main/java/subway/controller/ErrorInterceptor.java
@@ -0,0 +1,17 @@
+package subway.controller;
+
+import subway.view.OutputView;
+
+public class ErrorInterceptor {
+
+    public static void runUntilGetLegalArguments(Runnable menuService) {
+        while (true) {
+            try {
+                menuService.run();
+                return;
+            } catch (IllegalArgumentException exception) {
+                OutputView.printErrorMessage(exception.getMessage());
+            }
+        }
+    }
+}
