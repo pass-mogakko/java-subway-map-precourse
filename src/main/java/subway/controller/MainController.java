5b2a241cc33c9b2ff4db79b60a0f6cd63da4548e
swandevson <shai4956@gmail.com>
2022-12-06 01:34:11 +0900

    Feat(MainController): 메인메뉴 명령어 입력 및 명령어에 따른 분기처리

--- src/main/java/subway/controller/MainController.java
+++ src/main/java/subway/controller/MainController.java
@@ -1,5 +1,48 @@
 package subway.controller;
 
 
+import subway.command.MainCommand;
+import subway.view.InputView;
+
+
+
 public class MainController {
+    public void run() {
+        while(true) {
+            final MainCommand command = selectCommand();
+
+            if (command.equals(MainCommand.QUIT)) {
+                return;
+            }
+
+            if (command.equals(MainCommand.STATION)) {
+
+            }
+
+            if (command.equals(MainCommand.LINE)) {
+
+            }
+
+            if (command.equals(MainCommand.ROUTE)) {
+
+            }
+
+            if (command.equals(MainCommand.SHOW)) {
+                
+            }
+        }
+    }
+
+    private MainCommand selectCommand() {
+        while (true) {
+            try {
+                final String input = InputView.readMainMenu();
+                final MainCommand command = MainCommand.getCommand(input);
+
+                return command;
+            } catch (Exception exception) {
+                System.out.println(exception);
+            }
+        }
+    }
 }
