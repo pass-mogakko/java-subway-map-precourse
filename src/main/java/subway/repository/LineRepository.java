be23f40f17142f22ac75182b85b76b6912f822c9
jindaram-stu <jinsb1999@naver.com>
2022-12-06 05:43:23 +0900

    style: LineRepository 코드 구조 수정

---
+++ src/main/java/subway/repository/LineRepository.java
@@ -0,0 +1,37 @@
+package subway.repository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import subway.domain.line.Line;
+import subway.repository.dto.RouteMapDTO;
+
+public class LineRepository {
+    private static final List<Line> lines = new ArrayList<>();
+
+    public static List<Line> lines() {
+        return Collections.unmodifiableList(lines);
+    }
+
+    public static void addLine(Line line) {
+        lines.add(line);
+    }
+
+    public static boolean deleteLineByName(String name) {
+        return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static List<String> readLineNames() {
+        return lines.stream()
+                .map(line -> line.getName())
+                .collect(Collectors.toList());
+    }
+
+    public static List<RouteMapDTO> readRouteMaps() {
+        return lines.stream()
+                .map(line -> new RouteMapDTO(line.getName(), line.getSectionNames()))
+                .collect(Collectors.toList());
+    }
+}
