99689b69b9e69c5838233fc8e93e4236a6877397
jindaram-stu <jinsb1999@naver.com>
2022-12-06 05:43:52 +0900

    feat: 지하철 노선도 출력 기능 추가

--- src/main/java/subway/controller/function/RouteMapController.java
+++ src/main/java/subway/controller/function/RouteMapController.java
@@ -1,11 +1,17 @@
 package subway.controller.function;
 
 import subway.controller.ManagementController;
+import subway.service.LineService;
+import subway.view.output.RouteMapOutputView;
 
 public class RouteMapController implements ManagementController {
 
+    private final LineService lineService = new LineService();
+    private final RouteMapOutputView routeMapOutputView = new RouteMapOutputView();
+
     @Override
     public void execute() {
-        System.out.println("ROUTE");
+        System.out.println("푸하하");
+        routeMapOutputView.printRouteMaps(lineService.readRouteMaps());
     }
 }
---
+++ src/main/java/subway/view/output/RouteMapOutputView.java
@@ -0,0 +1,22 @@
+package subway.view.output;
+
+import java.util.List;
+import subway.constants.menu.View;
+import subway.repository.dto.RouteMapDTO;
+
+public class RouteMapOutputView {
+
+    public void printRouteMaps(List<RouteMapDTO> routeMaps) {
+        routeMaps.stream()
+                .forEach(routeMap -> printRouteMap(routeMap));
+    }
+
+    public void printRouteMap(RouteMapDTO routeMap) {
+        System.out.println(View.INFO + routeMap.getLineName());
+        System.out.println(View.INFO + View.LINE);
+        routeMap.getStationNames()
+                .stream()
+                .forEach(name -> System.out.println(View.INFO + name));
+    }
+
+}
