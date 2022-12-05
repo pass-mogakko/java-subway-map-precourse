44c694961e17293b60b07693689d9c3357410121
jindaram-stu <jinsb1999@naver.com>
2022-12-06 03:25:16 +0900

    refactor: 커맨드 입력 시 출력되는 예외 메시지 추가

---
+++ src/main/java/subway/constants/ExceptionMessage.java
@@ -0,0 +1,13 @@
+package subway.constants;
+
+public class ExceptionMessage {
+
+    private static final String PREFIX = "[ERROR] ";
+
+    public static final String CANT_CHOOSE = PREFIX + "선택할 수 없는 기능입니다.";
+    public static final String ALREADY_INSERT_STATION = PREFIX + "이미 등록된 역 이름입니다.";
+    public static final String ALREADT_INSERT_LINE = PREFIX + "이미 등록된 노선 이름입니다.";
+
+
+
+}
--- src/main/java/subway/view/validator/CommandValidator.java
+++ src/main/java/subway/view/validator/CommandValidator.java
@@ -2,6 +2,7 @@ package subway.view.validator;
 
 import java.util.Arrays;
 import java.util.List;
+import subway.constants.ExceptionMessage;
 import subway.constants.menu.Menu;
 
 public class CommandValidator {
@@ -9,9 +10,8 @@ public class CommandValidator {
     public static void validateCommand(String command, Menu menu) throws IllegalArgumentException {
         List<String> acceptCommands = Arrays.asList(menu.getAcceptCommand());
         if (!acceptCommands.contains(command)) {
-            throw new IllegalArgumentException("하하");
+            throw new IllegalArgumentException(ExceptionMessage.CANT_CHOOSE);
         }
     }
-
 
 }
