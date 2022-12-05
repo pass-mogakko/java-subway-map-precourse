d5d2beffab59b19b580c30bbe4c8379e8542cddf
swandevson <shai4956@gmail.com>
2022-12-06 02:27:33 +0900

    Feat(StationController): 추가/삭제 기능 구현

--- src/main/java/subway/controller/StationController.java
+++ src/main/java/subway/controller/StationController.java
@@ -1,4 +1,75 @@
 package subway.controller;
 
+import subway.command.StationCommand;
+import subway.domain.Station;
+import subway.domain.StationRepository;
+import subway.view.InputView;
+import subway.view.OutputView;
+
+
+
 public class StationController {
+    public void process() {
+        final StationCommand command = selectCommand();
+
+        if (command.equals(StationCommand.BACK)) {
+            return;
+        }
+
+        if (command.equals(StationCommand.ADD)) {
+            add();
+        }
+
+        if (command.equals(StationCommand.REMOVE)) {
+            remove();
+        }
+
+        if (command.equals(StationCommand.SHOW)) {
+
+        }
+    }
+
+    private StationCommand selectCommand() {
+        while(true) {
+            try {
+                final String input = InputView.readStationMenu();
+                final StationCommand command = StationCommand.getCommand(input);
+
+                return command;
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+    private Station add() {
+        while(true) {
+            try {
+                String input = InputView.readAddStation();
+                Station station = new Station(input);
+
+                StationRepository.addStation(station);
+                OutputView.printStationAddSuccess();
+
+                return station;
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+    private void remove() {
+        while (true) {
+            try {
+                String input = InputView.readRemoveStation();
+
+                StationRepository.deleteStation(input);
+                OutputView.printStationRemoveSuccess();
+
+                return;
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
 }
