64136730670b2ec0f43aac59019db98725e22dbd
송민진 <m_6595@naver.com>
2022-12-05 20:36:17 +0900

    feat(subway): 중복된 역이 없다면 지하철역 생성

--- src/main/java/subway/domain/station/StationService.java
+++ src/main/java/subway/domain/station/StationService.java
@@ -2,11 +2,28 @@ package subway.domain.station;
 
 import subway.domain.util.MessageFactory;
 
+import static subway.domain.util.ErrorCode.DUPLICATE_STATION_NAME;
+
 class StationService {
     private static final MessageFactory messageFactory = new MessageFactory();
 
-    public void addStation(String input) {
-
+    public void addStation(String name) {
+        validateNewName(name);
+        StationRepository.save(new Station(name));
     }
 
+    private void validateNewName(String name) {
+        Station station = StationRepository.findByName(name);
+        if (station != null) {
+            throw new IllegalArgumentException(messageFactory.makeErrorMessage(DUPLICATE_STATION_NAME));
+        }
+    }
+
+    private Station isPresentStation(String name) {
+        Station station = StationRepository.findByName(name);
+        if (station == null) {
+            throw new IllegalArgumentException();
+        }
+        return station;
+    }
 }
