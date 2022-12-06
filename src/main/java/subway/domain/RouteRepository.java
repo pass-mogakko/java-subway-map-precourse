285708244a374a7865d878553d63d4e499c219b8
swandevson <shai4956@gmail.com>
2022-12-06 23:10:26 +0900

    Refactor(RouteRepository): 초기 노선 생성 시, 역 이름을 unmodifable한 리스트로 전달하도록 변경

--- src/main/java/subway/domain/RouteRepository.java
+++ src/main/java/subway/domain/RouteRepository.java
@@ -11,9 +11,9 @@ public class RouteRepository {
     private static final List<Route> routes = new ArrayList<>();
 
     static {
-        routes.add(new Route("2호선", Arrays.asList("교대역", "강남역", "역삼역")));
-        routes.add(new Route("3호선", Arrays.asList("교대역", "남부터미널역", "양재역", "매봉역")));
-        routes.add(new Route("신분당선", Arrays.asList("강남역", "양재역", "양재시민의숲역")));
+        routes.add(new Route("2호선", List.of("교대역", "강남역", "역삼역")));
+        routes.add(new Route("3호선", List.of("교대역", "남부터미널역", "양재역", "매봉역")));
+        routes.add(new Route("신분당선", List.of("강남역", "양재역", "양재시민의숲역")));
     }
 
     public static List<Route> routes() {
