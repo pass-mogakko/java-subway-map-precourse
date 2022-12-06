27aadb52a5f792d2188cc7b9a7d6306d4a941c78
swandevson <shai4956@gmail.com>
2022-12-06 21:31:14 +0900

    Feat(LineController): 노선 추가/삭제/조회 기능 구현

--- src/main/java/subway/controller/LineController.java
+++ src/main/java/subway/controller/LineController.java
@@ -1,8 +1,12 @@
 package subway.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import subway.command.LineCommand;
-import subway.command.StationCommand;
+import subway.domain.Line;
+import subway.domain.LineRepository;
 import subway.view.InputView;
+import subway.view.OutputView;
 
 
 
@@ -15,15 +19,15 @@ public class LineController {
         }
 
         if (command.equals(LineCommand.ADD)) {
-            // TODO: 노선 추가
+            add();
         }
 
         if (command.equals(LineCommand.REMOVE)) {
-            // TODO: 노선 삭제
+            remove();
         }
 
         if (command.equals(LineCommand.SHOW)) {
-            // TODO: 노선 조회
+            show();
         }
     }
 
@@ -38,5 +42,39 @@ public class LineController {
                 System.out.println(exception.getMessage());
             }
         }
+    }
+
+    private Line add() {
+        while (true) {
+            try {
+                final String input = InputView.readAddLine();
+                final Line line = new Line(input);
+
+                LineRepository.addLine(line);
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+    private void remove() {
+        while (true) {
+            try {
+                final String input = InputView.readRemoveLine();
+
+                LineRepository.deleteLineByName(input);
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+    private void show() {
+        List<Line> lines = LineRepository.lines();
+        List<String> lineNames = lines.stream()
+                .map(Line::toString)
+                .collect(Collectors.toList());
+
+        OutputView.printLines(lineNames);
     }
 }
