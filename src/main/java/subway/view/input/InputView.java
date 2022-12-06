adcccebe0af782b1bf57a945fac2013a0534c3f9
jindaram-stu <jinsb1999@naver.com>
2022-12-07 01:52:08 +0900

    refactor: InputView, OutputView 인터페이스 추가
    추가못했던 부분 추가

--- src/main/java/subway/view/input/InputView.java
+++ src/main/java/subway/view/input/InputView.java
@@ -1,31 +1,7 @@
 package subway.view.input;
 
 import java.util.Scanner;
-import subway.constants.menu.Menu;
-import subway.domain.command.MainCommand;
-import subway.domain.command.ManageCommand;
-import subway.view.output.OutputView;
-import subway.view.validator.CommandValidator;
 
-public class InputView {
-
-    static final Scanner scanner = new Scanner(System.in);
-
-    public static MainCommand readMainMenu() {
-        OutputView.printMenu(Menu.MAIN);
-        String command = scanner.nextLine();
-        CommandValidator.validateCommand(command, Menu.MAIN);
-        return MainCommand.getCommand(command);
-    }
-
-    public static ManageCommand readManageMenu(Menu menu) {
-        OutputView.printMenu(menu);
-        String command = scanner.nextLine();
-        CommandValidator.validateCommand(command, menu);
-        return ManageCommand.getCommand(command);
-    }
-
-
-
+public interface InputView {
+    public static final Scanner scanner = new Scanner(System.in);
 }
-
