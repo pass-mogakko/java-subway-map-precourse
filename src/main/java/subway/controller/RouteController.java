85554b747e9c1ea14e287032b8863c853d9977bf
swandevson <shai4956@gmail.com>
2022-12-07 00:33:13 +0900

    Feat(RouteController): 커맨드 입력 및 분기 처리

--- src/main/java/subway/controller/RouteController.java
+++ src/main/java/subway/controller/RouteController.java
@@ -1,4 +1,37 @@
 package subway.controller;
 
+import subway.command.RouteCommand;
+import subway.view.InputView;
+
+
+
 public class RouteController {
+    public void process() {
+        RouteCommand command = selectCommand();
+
+        if (command.equals(RouteCommand.BACK)) {
+            return;
+        }
+
+        if (command.equals(RouteCommand.ADD)) {
+
+        }
+
+        if (command.equals(RouteCommand.REMOVE)) {
+
+        }
+    }
+
+    private RouteCommand selectCommand() {
+        while (true) {
+            try {
+                final String input = InputView.readLineMenu();
+                final RouteCommand command = RouteCommand.getCommand(input);
+
+                return command;
+            } catch (Exception exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
 }
