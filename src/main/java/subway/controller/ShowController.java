40a26994935524e039a1939980f636b8b0f220b3
swandevson <shai4956@gmail.com>
2022-12-07 20:40:12 +0900

    Add(ShowController): 노선도 조회 controller 생성

---
+++ src/main/java/subway/controller/ShowController.java
@@ -0,0 +1,17 @@
+package subway.controller;
+
+import subway.domain.Route;
+import subway.domain.RouteRepository;
+import subway.view.OutputView;
+
+import java.util.List;
+
+public class ShowController implements Controllable {
+    public void process() {
+        List<Route> routes = RouteRepository.routes();
+
+        for (Route route : routes) {
+            OutputView.printRoute(route.getLineName(), route.getStationNames());
+        }
+    }
+}
