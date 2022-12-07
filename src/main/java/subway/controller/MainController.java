63bd8d3eca948e1ebfef7523893d3e97d3bab6af
swandevson <shai4956@gmail.com>
2022-12-08 02:57:43 +0900

    Refactor(MainController): 입력 커맨드와 그에 대한 함수형 인터페이스 enum 클래스로 분리

--- src/main/java/subway/controller/MainController.java
+++ src/main/java/subway/controller/MainController.java
@@ -9,17 +9,6 @@ import java.util.Map;
 
 
 public class MainController {
-    private final Map<MainCommand, Controllable> mainControllers;
-
-    public MainController() {
-        mainControllers = new HashMap<>();
-
-        mainControllers.put(MainCommand.STATION, new StationController());
-        mainControllers.put(MainCommand.LINE, new LineController());
-        mainControllers.put(MainCommand.ROUTE, new RouteController());
-        mainControllers.put(MainCommand.SHOW, new ShowController());
-    }
-
     private MainCommand selectCommand() {
         final String input = Repeater.repeatInput(InputView::readMainMenu);
 
@@ -30,7 +19,7 @@ public class MainController {
         MainCommand command = Repeater.repeatInput(this::selectCommand);
 
         while (!command.equals(MainCommand.QUIT)) {
-            Controllable controllable = mainControllers.get(command);
+            Controllable controllable = ManageController.getController(command);
             controllable.process();
 
             command = Repeater.repeatInput(this::selectCommand);
